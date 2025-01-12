554823d548588a95fe15caa448f827b1ec993585
Apostolos081 <apostolostheophaneskyparisses@gmail.com>
2025-01-12 12:17:36 +0200

    Add files via upload

---
+++ functionsdeclarations03.cpp
@@ -0,0 +1,6 @@
+void initialize_catalog();
+void initialize_variables();
+void client_working();
+void server_working();
+void print_results_orders(int failed_orders,int succesfull_orders,int total);
+void print_results_requests(int succesfull_requests,int failed_requests);
