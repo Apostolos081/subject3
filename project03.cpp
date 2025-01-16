f95440cbf4d80126fb87687a201a788785c764cb
Apostolos081 <apostolostheophaneskyparisses@gmail.com>
2025-01-16 09:54:06 +0200

    Add files via upload

---
+++ project03.cpp
@@ -0,0 +1,32 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <sys/socket.h>
+#define SOCKETNAME "MySocket"
+#define NUM_CLIENTS 5
+#define NUM_PRODUCTS 20
+#define MAX_ORDERS 10
+#define <fuctionsbodies03.cpp>
+#define <functionsdeclarations03.cpp>
+
+typedef struct products{
+	char description[50];
+	int price;
+	int item_count = 2;
+}products;
+
+products catalog[NUM_PRODUCTS];
+
+
+int main(void){
+	initialize_catalog();
+	initialize_variables();
+	client_working();
+	server_working();
+	return 0;
+	
+}
